c2ee10c2aef3098e17a2d8eb71bcb0693252af17
Max Beier <beierm@htw-berlin.de>
2015-05-03 12:51:44 +0800

    Prettier grid

--- views/footer.php
+++ views/footer.php
@@ -1,17 +1,19 @@
 
-<footer class="row">
-    <ol class="breadcrumb">
-        <li class="active">
-            <a href="">Home</a>
-        </li>
-        <li>
-            <a href="mailto:florian.willich@tu-berlin.de?subject=NERD%20Talk">Contact</a>
-        </li>
-        <li>
-            Technische Universit&auml;t zu Berlin, Berlin, Germany &copy; 2014 - 2015
-        </li>
-    </ol>
-</footer>
+<div class="row">
+    <footer class="col-md-12">
+        <ol class="breadcrumb">
+            <li class="active">
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="mailto:florian.willich@tu-berlin.de?subject=NERD%20Talk">Contact</a>
+            </li>
+            <li>
+                Technische Universit&auml;t zu Berlin, Berlin, Germany &copy; 2014 - 2015
+            </li>
+        </ol>
+    </footer>
+</div>
 
 </div> <!-- / .container -->
 </body>
--- views/talks/list.php
+++ views/talks/list.php
@@ -1,4 +1,4 @@
-<div class="row list-group talks">
+<div class="talks row">
 
    <?php echo Message::show(); ?>
 
@@ -9,14 +9,14 @@
       else {
          foreach ($data['talks'] as $talk) {
             echo
-            '<div class="item col-xs-12 col-md-4">
-               <div class="thumbnail" style="background-image: url(' . $talk['image'] . ')">
+            '<div class="item col-md-4">
+               <div class="talk thumbnail" style="background-image: url(' . $talk['image'] . ')">
                   <div class="buttons-edit">
                      <a class="btn btn-default btn-sm" href="' . DIR . 'talks/edit/' . $talk['id'] . '">Edit</a>
                   </div>
-                  <div class="caption clearfix">
-                     <h4><a href="' . $talk['url'] . '" title="' . $talk['title'] . '">' . $talk['title'] . '</a></h4>
-                     <p>' . $talk['description'] . '</p>
+                  <div class="talk-caption">
+                     <h4 class="talk-title"><a href="' . $talk['url'] . '" title="' . $talk['title'] . '">' . $talk['title'] . '</a></h4>
+                     <p class="talk-description">' . $talk['description'] . '</p>
                      <p><b>' . $talk[host] . '</b></p>
                      <span class="lead">' . $talk['votes'] . ' Votes</span>
                      <a class="btn btn-default pull-right" href="' . DIR . 'talks/vote/' . $talk['id'] . '">Vote</a>
