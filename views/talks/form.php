e2f459117d5cea48fa8f440043c0e6d14e0b2f23
TU <gabriel.reimers@tu-berlin.de>
2015-05-11 18:05:00 +0200

    - added textarea for description - added better placeholders for edit form - changed indentation

--- views/talks/form.php
+++ views/talks/form.php
@@ -1,48 +1,59 @@
 <div class="panel panel-default">
 
-   <div class="panel-heading">
-      <h3 class="panel-title"><?= $data['form_header'] ?></h3>
-   </div>
+    <div class="panel-heading">
+        <h3 class="panel-title"><?= $data['form_header'] ?></h3>
+    </div>
 
-   <div class="panel-body">
+    <div class="panel-body">
 
-      <?php echo Message::show(); ?>
+        <?php echo Message::show(); ?>
 
-      <?php
-         $talk = $data['talk'];
-         if (isset($talk['id'])) :
-      ?>
+        <?php
+        $talk = $data['talk'];
+        if (isset($talk['id'])) :
+            ?>
 
-      <form role="form" action="<?= DIR ?>talks/insert" method="POST">
-         <input class="form-control" type="text" name="title" placeholder="Title" value="<?= $talk['title'] ?>">
-         <input class="form-control" type="text" name="description" placeholder="Description" value="<?= $talk['description'] ?>">
-         <input class="form-control" type="text" name="host" placeholder="Host" value="<?= $talk['host'] ?>">
-         <input class="form-control" type="url" name="url" placeholder="URL" value="<?= $talk['url'] ?>">
-         <input class="form-control" type="url" name="image" placeholder="Image" value="<?= $talk['image'] ?>">
-         <div class="row">
-            <div class="col-xs-6">
-               <button type="submit" class="btn btn-primary btn-block">Update</button>
-            </div>
-         </div>
-         <input type="hidden" name="id" value="<?= $talk['id'] ?>">
-      </form>
+            <form role="form" action="<?= DIR ?>talks/insert" method="POST">
+                <input class="form-control" type="text" name="title" placeholder="Title" value="<?= $talk['title'] ?>">
+                <textarea class="form-control" type="text" name="description" placeholder="Description"
+                          rows="4"><?= $talk['description'] ?></textarea>
+                <input class="form-control" type="text" name="host" placeholder="Host" value="<?= $talk['host'] ?>">
+                <input class="form-control" type="url" name="url" placeholder="URL" value="<?= $talk['url'] ?>">
+                <input class="form-control" type="url" name="image" placeholder="Image" value="<?= $talk['image'] ?>">
 
-      <?php else : ?>
+                <div class="row">
+                    <div class="col-xs-6">
+                        <button type="submit" class="btn btn-primary btn-block">Update</button>
+                    </div>
+                </div>
+                <input type="hidden" name="id" value="<?= $talk['id'] ?>">
+            </form>
 
-      <form role="form" action="<?= DIR ?>talks/insert" method="POST">
-         <input class="form-control" type="text" name="title" placeholder="Title" value="<?= $talk['title'] ?>">
-         <input class="form-control" type="text" name="description" placeholder="Description" value="<?= $talk['description'] ?>">
-         <input class="form-control" type="text" name="host" placeholder="Host" value="<?= $talk['host'] ?>">
-         <input class="form-control" type="url" name="url" placeholder="URL" value="<?= $talk['url'] ?>">
-         <input class="form-control" type="url" name="image" placeholder="Image" value="<?= $talk['image'] ?>">
-         <div class="row">
-            <div class="col-xs-6">
-               <button type="submit" class="btn btn-primary btn-block">Create</button>
-            </div>
-         </div>
-      </form>
+        <?php else : ?>
 
-   <?php endif; ?>
+            <form role="form" action="<?= DIR ?>talks/insert" method="POST">
+                <input class="form-control" type="text" name="title" placeholder="Title of Talk" value="<?= $talk['title'] ?>">
+                <textarea class="form-control" type="text" name="description" placeholder="Description"
+                          rows="4"><?= $talk['description'] ?></textarea>
+                <input class="form-control" type="text" name="host" placeholder="Host Name" value="<?= $talk['host'] ?>">
+                <input class="form-control" type="url" name="url" placeholder="Info URL" value="<?= $talk['url'] ?>">
 
-   </div> <!-- / .panel-body -->
+                <div class="form-group">
+
+                    <input class="form-control" type="url" name="image" placeholder="Image URL"
+                           value="<?= $talk['image'] ?>">
+                    <p class="help-block">Please use a <a href="http://search.creativecommons.org/" target="_blank">creative commons image</a>.</p>
+
+                </div>
+                <div class="row">
+                    <div class="col-xs-6">
+                        <button type="submit" class="btn btn-primary btn-block">Create</button>
+                    </div>
+                </div>
+            </form>
+
+        <?php endif; ?>
+
+    </div>
+    <!-- / .panel-body -->
 </div> <!-- / .panel -->
